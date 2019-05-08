dc5d68965e52e3a6a8f5ca4a755a38549c087bea
aewaddell <50387111+aewaddell@users.noreply.github.com>
2019-05-07 22:32:42 -0400

    Add files via upload

---
+++ excercises/ex3.py
@@ -0,0 +1,32 @@
+# print a string
+print("I will now count my chickens:")
+
+# print string and then print result of mathematical operations
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# conduct both sides of mathematical equations, compare and print bool result
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+# print string and then print output of bool comparison
+print("Is it greater?", 5 >= -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+#STUDY DRILLS
+# 4 - change to floating point by converting equators to floating point
+# % character is a modulus character - returns remaining number after division
+# order of operations 
