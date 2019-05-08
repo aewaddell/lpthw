2027a8c31c9118e8847f996785b8bc6c1f70f065
aewaddell <50387111+aewaddell@users.noreply.github.com>
2019-05-07 23:33:50 -0400

    Add files via upload

---
+++ excercises/ex4.py
@@ -0,0 +1,20 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+print("There are only", drivers, "drivens available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
+
+# STUDY DRILLS
+# because you initiliazed carpool_capacity not car_pool_capacity
+# 1. it will change the output from int to floating point
+# '=' gives data names, '==' test whether two things have same value 
---
+++ excercises/ex5.py
@@ -0,0 +1,26 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is trick, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height} and {weight} I get {total}.")\
+
+#STUDY DRILLS
+#1. find and remove (cntrl f)
+
+inches = 1
+cm = 2.54 * pounds
+kg = 1
+lbs = 2.205 * kg
