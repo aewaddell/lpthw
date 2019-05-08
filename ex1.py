864c2cb85f2899f9b61e34f6a142f65aa0018973
aewaddell <50387111+aewaddell@users.noreply.github.com>
2019-05-07 21:56:01 -0400

    Add example 1

---
+++ ex1.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this")
+print("This is fun")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+#run this code on terminal (SAVE before running)
+
+#STUDY DRILLS
+print("Another Line") #1
+#2: print only one line (delete or comment out)
+# this turns lines into comments - this code does not run
